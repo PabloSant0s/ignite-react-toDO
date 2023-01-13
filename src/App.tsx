f7b611e6b735ef8ee38eb418bc70f79931334261
PabloSant0s <pablorasantos.dev@gmail.com>
2023-01-13 08:44:31 -0300

    [feat]Form New Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { PlusCircle } from 'phosphor-react'
 import styles from './App.module.css'
 import { Header } from './components/Header/Header'
 
@@ -5,6 +6,15 @@ export function App() {
   return (
     <div className={styles.container}>
       <Header/>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type="text" placeholder='Adicione uma nova tarefa'/>
+          <button>
+            <span>Criar</span>
+            <PlusCircle size={16}/>
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
